6dbbab8cff370f0bd2060227a6fab9c257268732
miriameley <mre38@cam.ac.uk>
2022-01-31 20:44:35 +0000

    function for 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -28,3 +28,14 @@ def stations_by_distance(stations, p):
 
     return sorted_name_distance
 
+def stations_within_radius(stations, centre, r):
+    """returns a list of all stationa within radius r of a geographic coordinate x"""
+    list_within_radius=[] #opens a list
+    for station in stations: #iterate through stations
+        coord = station.coord #forms a tuple of (... , ...)
+        name = station.name #forms a string name
+        x = centre 
+        distance = float(haversine(coord, x)) #calculates the distance between the station and the centre (x) as a float
+        if distance <= r: #if station is within the radius
+            list_within_radius.append(name) #adds the name of the station to a list
+    return list_within_radius
