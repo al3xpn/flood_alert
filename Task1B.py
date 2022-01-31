7f48dd43eed0bdf7a74bbc0d9d1562c428af423f
Alex Perrin <adperrin2712@gmail.com>
2022-01-31 21:24:39 +0000

    still working on 1B

--- Task1B.py
+++ Task1B.py
@@ -1,15 +1,27 @@
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
-stations = build_station_list()
-
-print(stations_by_distance(stations, (52.188229, 0.102169)))
-
+from floodsystem.station import MonitoringStation
 
 def run():
-    #
+    """Prints a list of tuples (station name, town, distance) for the 10 closest and the 10 furthest stations from the Cambridge city centre, (52.2053, 0.1218)."""
+    stations = build_station_list()  #builds list of stations
+    towns=[]
+    for station in stations:
+        towns.append(station.town)
+    
+    p = (52.2053, 0.1218)    #coordinate for cambridge city centre
 
+    full_list= stations_by_distance(stations, p)   #full list of all mintoring stations, ordered with closest to cty centre
+    closest_10= full_list[:10]        #10 closest stations to city centre
+    furthest_10= full_list[-10:]       #10 furthest stations from city centre
+    for n in closest_10:
+        closest_10.append(towns[n])
+
+    #town = station.town
+    #station[1] = town
+    #print(full_list)
+    print(closest_10)
 
 if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***")
     run()
-
-print(len(stations_by_distance(stations, (52.188229, 0.102169))))
--- test_geo.py
+++ test_geo.py
@@ -2,13 +2,12 @@
 
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
-stations = build_station_list()
 
 def test_stations_by_distance():
+    p = (52.188229, 0.102169)
     stations = build_station_list()
-    sorted_distance_list = stations_by_distance()
+    sorted_distance_list = stations_by_distance(stations, p)
     assert len(sorted_distance_list) == 2165  #number of measuring stations it should return
 
-    p = (52.188229, 0.102169)
     sorted_distance_list = stations_by_distance(stations, p)
     assert sorted_distance_list[0][0] == 'Bin Brook'
