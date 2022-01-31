47a4b8608c3f8714e21a531d5dae494087c2f03b
miriameley <mre38@cam.ac.uk>
2022-01-31 21:17:27 +0000

    1C

--- Task1C.py
+++ Task1C.py
@@ -3,6 +3,6 @@ from floodsystem.stationdata import build_station_list
 stations = build_station_list()
 
 centre = (52.2053, 0.1218)
-stations_in_ten = stations_within_radius(stations, centre, 10000)
+stations_in_ten = stations_within_radius(stations, centre, 10)
 stations_in_ten.sort()
 print(stations_in_ten)
