20aad2949322a9bc81d47940eaf8d604b518acdb
Alex Perrin <adperrin2712@gmail.com>
2022-02-01 18:21:20 +0000

    Finished 1B to give required output using the function made in geo

--- Task1B.py
+++ Task1B.py
@@ -12,23 +12,26 @@ def run():
     full_list= stations_by_distance(stations, p)   #full list of all mintoring stations, ordered with closest to cty centre
     closest_10= full_list[:10]        #10 closest stations to city centre
     furthest_10= full_list[-10:]       #10 furthest stations from city centre
-    
-    #x = list(closest_10[4])
-    #print(x)
-    #x.insert(1, 'Test')
-    #print(x)
-    #print(tuple(x))
-    #closest_10[5] = tuple(x)
-    #print(closest_10)
 
     for n in range(len(closest_10)):
-        single_list = list(closest_10[n]) #was tuple before, turn into list so can insert
-        single_list.insert(1, 'Town')
-        single_tuple=tuple(single_list)
-        closest_10[n] = single_tuple
+        for station in stations:
+            if station.name == closest_10[n][0]:   #only iterates over stations in the top 10 closest
+                single_list = list(closest_10[n]) #was tuple before, turn into list so can insert town name
+                single_list.insert(1, station.town)   #inserts town name into list between name and distance
+                single_tuple=tuple(single_list)     #turn it back to tuple
+                closest_10[n] = single_tuple       #replace item in list
     print(closest_10)
+
+    for n in range(len(furthest_10)):
+        for station in stations:
+            if station.name == furthest_10[n][0]:   #iterates over top 10 furthest
+                single_list = list(furthest_10[n]) #was tuple before, turn into list so can insert
+                single_list.insert(1, station.town)  
+                single_tuple=tuple(single_list)
+                furthest_10[n] = single_tuple
+    print(furthest_10)
 
 
 if __name__ == "__main__":
-    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
     run()
