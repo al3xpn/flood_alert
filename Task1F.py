a9f66cf6114e72b3a83404ac5f37d4ac496462f3
Alex Perrin <adperrin2712@gmail.com>
2022-02-15 19:16:48 +0000

    Merge branch 'main' of https://github.com/al3xpn/flood_alert

--- Task1F.py
+++ Task1F.py
@@ -4,9 +4,9 @@ from floodsystem.stationdata import build_station_list
 def run():
     """Builds a list of all stations with inconsistent typical range data. Print a list of station names, in alphabetical order, for stations with inconsistent data."""
 
-    stations = build_station_list()
+    stations = build_station_list()       
     in_list = inconsistent_typical_range_stations(stations)
-    in_list.sort()
+    in_list.sort()     #sorts the list alphabetically
 
     print(in_list)
 
