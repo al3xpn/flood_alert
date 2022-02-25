2c7cc741454153399d8a3ab43ffab3b87e049432
Alex Perrin <adperrin2712@gmail.com>
2022-02-25 11:49:27 +0000

    Made 2E show graphs for test data

--- Task2F.py
+++ Task2F.py
@@ -1,5 +1,21 @@
+
+
+
+
+from floodsystem.plot import plot_water_level_with_fit
+from floodsystem.stationdata import build_station_list
+from floodsystem.datafetcher import fetch_measure_levels
+from datetime import datetime
+
 def run():
-    """"""
+    """""" 
+    stations = build_station_list()
+    dt = 2
+    for station in stations:
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+
+    plot_water_level_with_fit
+
 
 if __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
