7c79b0175f21666213320486310622821f721481
Alex Perrin <adperrin2712@gmail.com>
2022-03-02 14:31:31 +0000

    added comments to my code and tried making a test for polyfit (not done)

--- Task2F.py
+++ Task2F.py
@@ -9,19 +9,25 @@ import datetime
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.analysis import polyfit
 def run():
-    """""" 
+    """Plots graphs for the 5 stations with highest current relative water level. Plots the level data extending back 2 days. Also plots high/low range and best fit of polynomial of degree 4 against time.""" 
     stations = build_station_list()
     update_water_levels(stations)
-    dt = 2
-    top_5 = stations_highest_rel_level(stations, 5)
-    top_5_stations = []
+    dt = 2   #number of days extending back
+    top_5 = stations_highest_rel_level(stations, 5)   #gets list of tuples for top 5 stations with highest relative water level , tuple = (station (object), relative level)
+    top_5_stations = []   #create empty list to put just the station objects into
+
+    xtest = [1, 2, 3, 4, 5, 6, 7]
+    ytest = [1, 4, 9, 16, 25, 36, 49]
+    poly, d0 = polyfit(xtest, ytest, 4)
+    print(poly(3))
 
     for n in range(len(top_5)):
-        top_5_stations.append(top_5[n][0])
+        top_5_stations.append(top_5[n][0])    #puts just the station objects in the list
 
     for station in top_5_stations:
-        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-        plot_water_level_with_fit(station, dates, levels, 4)
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))   #gathers dates and the levels at those dates
+        plot_water_level_with_fit(station, dates, levels, 4)    #plots graph
+        
     
 if __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -3,15 +3,15 @@ import matplotlib
 from floodsystem.analysis import polyfit
 import numpy as np
 def plot_water_levels(station, dates, levels):
-    """"""
-    low_range_list = [station.typical_range[0]] * len(dates)
+    """For 2E - Plots graph for data and low/high ranges against time"""
+    low_range_list = [station.typical_range[0]] * len(dates) #create a list of numbers (typical low level) that is the same length of the list of dates so can be plotted together
     high_range_list = [station.typical_range[1]] * len(dates)
-    plt.plot(dates, low_range_list, label = "Low range")
+    plt.plot(dates, low_range_list, label = "Low range")    #just plotting the values
     plt.plot(dates, high_range_list, label = "High range")
-    plt.plot(dates, levels , label = "Historical Data")
+    plt.plot(dates, levels , label = "Historical Data")    #historical data (stuff that changes)
 
-    plt.legend(loc='upper right')
-    plt.xlabel('Date/Time')
+    plt.legend(loc='upper right')      #added lables to show which line is which
+    plt.xlabel('Date/Time(Year-Month-Day)')
     plt.ylabel('Water level (m)')
     plt.title(station.name)
     plt.xticks(rotation=45)
@@ -21,21 +21,20 @@ def plot_water_levels(station, dates, levels):
 
 
 def plot_water_level_with_fit(station, dates, levels, p):
-    """"""
+    """For 2F - Plots graph for data, low/high ranges and polynomial best fit against time"""
     poly, d0 = polyfit(dates, levels, p)
 
-    low_range_list = [station.typical_range[0]] * len(dates)
+    low_range_list = [station.typical_range[0]] * len(dates)  #once again creating list of the different levels
     high_range_list = [station.typical_range[1]] * len(dates)
-    x = matplotlib.dates.date2num(dates)
-    x = x - x[0]
-    x1 = np.linspace(x[0], x[-1], len(dates))
-    plt.plot(dates, poly(x1), label = "Best fit polynomial")
+    x = matplotlib.dates.date2num(dates)    #creating list of dates that are now float values, not datetime values
+    x = x - x[0]      #as they are now just large floats, i have take away the first value so that the list starts at 0 (in time) and increases from there
+    plt.plot(dates, poly(x), label = "Best fit polynomial")   #plot dates against the polynomial values (think of poly as just y= x^4 + 4x^3 + ... (for example) for different values of x)
     plt.plot(dates, levels, label = "Historical Data")
     plt.plot(dates, low_range_list, label = "Low range")
     plt.plot(dates, high_range_list, label = "High range")
 
     plt.legend(loc='upper left')
-    plt.xlabel('Date/Time')
+    plt.xlabel('Date/Time(Month-Day-Hour)')
     plt.ylabel('Water level (m)')
     plt.title(station.name)
     plt.xticks(rotation=45)
