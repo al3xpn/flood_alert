6ed5b7401c7ecf9e68fd48b553e6c5ab09338578
Alex Perrin <adperrin2712@gmail.com>
2022-03-02 19:34:13 +0000

    added more comments

--- Task2E.py
+++ Task2E.py
@@ -9,16 +9,16 @@ def run():
     """Plots a graph of the water levels against time for the past 10 days of the 5 stations which the current relative water level is greatest."""
     stations = build_station_list()
     update_water_levels(stations)
-    dt = 10
-    top_5 = stations_highest_rel_level(stations, 5)
+    dt = 10     #number of days to extend back to
+    top_5 = stations_highest_rel_level(stations, 5)    #getting top 5 stations with highest relative level, list of tuples: tuple = (station(object), relative level)
     top_5_stations = []
 
     for n in range(len(top_5)):
-        top_5_stations.append(top_5[n][0])
+        top_5_stations.append(top_5[n][0])      #once again just create a list of the top 5 station objects
 
     for station in top_5_stations:
         dates , levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-        plot_water_levels(station, dates, levels)
+        plot_water_levels(station, dates, levels)   #plots graph of levels against time, including lines for typical high/low ranges
 
 
 
