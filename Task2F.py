d48bab974c9e98d03aa2aee72ad2db47b740ab6e
Alex Perrin <adperrin2712@gmail.com>
2022-03-02 13:15:37 +0000

    finished 2F

--- Task2F.py
+++ Task2F.py
@@ -3,20 +3,26 @@
 
 
 from floodsystem.plot import plot_water_level_with_fit
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
-from datetime import datetime
-
+import datetime
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.analysis import polyfit
 def run():
     """""" 
     stations = build_station_list()
+    update_water_levels(stations)
     dt = 2
-    for station in stations:
+    top_5 = stations_highest_rel_level(stations, 5)
+    top_5_stations = []
+
+    for n in range(len(top_5)):
+        top_5_stations.append(top_5[n][0])
+
+    for station in top_5_stations:
         dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-
-    plot_water_level_with_fit
-
-
+        plot_water_level_with_fit(station, dates, levels, 4)
+    
 if __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
     run()
