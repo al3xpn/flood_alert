fe169e64607802cc11723d9a1a1fdf9e21459f10
Alex Perrin <adperrin2712@gmail.com>
2022-03-03 13:33:22 +0000

    Made a successful test for polyfit

--- Task2F.py
+++ Task2F.py
@@ -1,7 +1,3 @@
-
-
-
-
 from floodsystem.plot import plot_water_level_with_fit
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
--- test_analysis.py
+++ test_analysis.py
@@ -1,19 +1,19 @@
 import datetime
 from floodsystem.analysis import polyfit
+from floodsystem.stationdata import update_water_levels, build_station_list
+from floodsystem.datafetcher import fetch_measure_levels
+import random 
 
 def test_polyfit():
     """Tests the function polyfit used for 2F."""
+    stations = build_station_list()
+    update_water_levels(stations)
+    dt=2
+    station = stations[(random.randint(1, len(stations)))]          #gets a random station from the list of stations
+    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))            #gathers dates and levels for that station
+    poly, d0 = polyfit(dates, levels, 4)                #forms the polynomial of best fit for the given data
 
-    x = [0.0, 1.0, 2.0, 3.0,  4.0,  5.0]
-    y = [0.0, 0.8, 0.9, 0.1, 2.0, 1.0, 1.5]
+    assert float('%.1g' % poly(0)) == float('%.1g' % station.latest_level)     #asserts that the result from the polynomial is equal to the relative water level at that time (to 1 sig fig) e.g. current time
 
-    #poly, d0 = polyfit(x, y, 4)
-    
-    #assert poly(3) == 1
-
-    assert 1 == 1
-    #assert float('%.2g' % poly(4)) == -0.8
-
-    #assert float('%.2g' % poly(5)) == -1.0
 
 
